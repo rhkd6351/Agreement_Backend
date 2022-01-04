88e2eb01437138477a149f0d1184ddbbfb6e20d1
Lim_Yekwang <56215323+rhkd6351@users.noreply.github.com>
2022-01-04 16:22:14 +0900

    Init Project

---
+++ src/test/java/com/curioud/signclass/SignclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curioud.signclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
